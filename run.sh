e92b502e0e934dd55da9bb67a8de0b103b636d99
Mathieu Buoro <matbuoro@gmail.com>
2018-02-06 19:27:43 +0100

    create straying matrix

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
-# ! /bin/sh
+#!/bin/sh
 #$ -S /bin/sh
 
-
-npop=3
+# Parameters
+npop=4
 nyears=10
 
 for pop in $(seq 1 $npop)
 do
+
+# Create straying matrix
+cp R/stray.R R/stray_$npop.R
+sed 's|npop|'"$npop"'|g' -i R/stray_$npop.R
 
 # Create R script for each populations
 cp R/rIBASAM.R R/rIBASAM_$pop.R
@@ -18,6 +22,13 @@ sed 's|nYears|'"$nyears"'|g' -i R/rIBASAM_$pop.R
 mkdir -p tmp/
 
 # Run analysis
+R CMD BATCH --no-save --no-restore --slave R/stray.R R/stray.Rout &
+
+# Run analysis
 R CMD BATCH --no-save --no-restore --slave R/rIBASAM_$pop.R R/rIBASAM_$pop.out &
+
+# Cleaning
+rm -f rIBASAM_$pop.R
+#rm -f rIBASAM_$pop.out
 
 done
