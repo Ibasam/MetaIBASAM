c2f03ef92b79d839e3c212620843ccce5332b65a
Mathieu Buoro <matbuoro@gmail.com>
2018-02-07 13:56:15 +0100

    moved

--- R/scriptR.R
+++ R/scriptR.R
@@ -8,7 +8,7 @@ library(Ibasam)
 #Sys.setlocale('LC_ALL','C') 
 #source(paste("scriptIBASAM","vIBASAM","iSIMUL","_TMP.R",sep=""))
 #source(paste("demoIbasam","iSIMUL","_TMP.R",sep=""))
-source("R/demoIbasam.R")
+source("demoIbasam.R")
 
 ## Run simualtions
 #RES <- list()
@@ -17,16 +17,16 @@ source("R/demoIbasam.R")
                     , npop = Npop # Number of popualtions
                     , Pop.o = popo # Popualtion of origin
                     , rPROP = .1 # Popualtion size (1/10 of production area)
-                    , pstray = 0.1
+                    , pstray = 0.5
                     , CC.Temp=tempCC # Water Temperature increase (keep constant if 0)
                     , CC.Amp=ampCC # Flow amplitude increase (keep constant if 1)
                     , CC.Sea=seaCC #.75 # decresaing growth condition at sea (seqeunce from 1 to .75; keep constant if 1)
                     , fisheries=fish.state, stage = fish.stage, fishing_rate=c(.15,.15,.15)
                     , returning=TRUE # if TRUE, return R object contaning all data (very long)
-                    , plotting=FALSE,window=FALSE,success=FALSE,empty=TRUE
+                    , plotting=TRUE,window=FALSE,success=FALSE,empty=TRUE
   )
   #RES <- RES[RES$year>init,]
-  save(RES,file=paste0("R/RESsimul_",popo,".RData"))
+  save(RES,file=paste0("tmp/RESsimul_",popo,".RData"))
   #save(RES,file=paste0("RESsimul_",popo,"_iSIMUL","-",i,".RData"))
 #}
 #save(RES,file=paste("RESsimul",iSIMUL,".RData",sep=""))
