8e254559e9a24b53e8e4221d567b37ca0bcd65fa
Mathieu Buoro <matbuoro@gmail.com>
2018-04-12 11:23:41 +0200

    check if execution halted in .Rout files

--- analysis/RT.sh
+++ analysis/RT.sh
@@ -3,11 +3,11 @@
 
 # Parameters:
 npop=16
-nSIMUL=1 # Nb simulations 100
-Init=5 # Nb years to initialized 15
-Years=20 # Nb years simulated 50
-rPROP_RT=0.1 # proportion (population size) 20% de l'aire
-maxtime=5 # time of each simualtion (minutes)
+nSIMUL=30 # Nb simulations 100
+Init=15 # Nb years to initialized 15
+Years=50 # Nb years simulated 50
+rPROP_RT=0.2 # proportion (population size) 20% de l'aire
+maxtime=40 # time of each simualtion (minutes)
 
 # Fishing:
 state=TRUE # TRUE If fishing applied
@@ -34,7 +34,7 @@ stage=TRUE # fishing on life stages (1SW/MSW) if TRUE, on Sizes ("small","med","
 scenarioConnectRT=3 #scenario 1 pour h=1.00, scenario 2 pour h=0.942, scenario 3 pour h=0.80
 
 # Environmental conditions:
-scenarioEnviRT=2 #scenario 1 pour absence de CC, scenario 2 pour CC
+scenarioEnviRT=1 #scenario 1 pour absence de CC, scenario 2 pour CC
 #Temp=0 # Water Temperature (T° increase / Years; keep constant if 0)
 #Amp=1 # Flow amplitude (keep constant if 1)
 #Sea=1 # decreasing growth condition at sea (keep constant if 1)
@@ -51,6 +51,7 @@ if [ ! -d "$scenario" ]; then
   cp scriptR.R $scenario
   cp demoIbasam.R $scenario
   cp Matrices_Laplace_AireLog.RData $scenario
+  cp rename.sh $scenario
 fi
 
 cd $scenario # move directory to scenario folder
@@ -108,19 +109,21 @@ wait
 ##ls scriptR_{1..16}.R|xargs -n 1 -P 0 R --vanilla
 ##parallel -j0 R --vanilla ::: scriptR_{1..16}.R
 ##(Rscript --vanilla tmp/scriptR_1.R) | parallel
-#declare -a PIDS
+declare -a PIDS
+#declare -a PIDS2
 for pop in $(seq 1 $npop)
 do
 R CMD BATCH --vanilla scriptR_$pop.R &
 #sleep 1
-#PIDS+=("$!") # add PID du processus lance dans l'array
+PIDS+=("$!") # add PID du processus lance dans l'array
+#PIDS2+=(pgrep -P "$!")
 #echo "${PIDS[@]}"
+#echo "${PIDS2[@]}"
 #echo "PID du processus courant : $$"
 #echo "PID du processus lancé : $!"
 ## rm -f scriptR_$pop.out
 ## rm -f scriptR_$pop.R
 done # end loop pop
-
 
 
 
@@ -135,39 +138,54 @@ done # end loop pop
 #     echo "Simulation $s failed"
 #     pkill -2 R # kill all R processes
 # fi
+# cd .. # return to main directory
 
 DIR="results/" # directory where to save results
 STARTTIME=$(date +%s)
 while [ ! "$(ls -A $DIR)" ] # check if DIR is empty
 #while [ ! -f "$DIR/RES_Pop-14.RData" ]
 do
-  #duration=$COUNT #(($SECONDS / 60))
-  #echo "$COUNT"
+
+  ## test if execution halted in .Rout files
+  for pop in $(seq 1 $npop)
+  do
+    File="scriptR_$pop.Rout"
+    if grep -q "Execution halted" "$File"; then
+      echo "Population $pop : Execution halted"
+      echo "Re-launch $File"
+      R CMD BATCH --vanilla scriptR_$pop.R &
+      PIDS[$pop]=("$!")
+    fi
+  done
+
+  ## Test if analysis are too long
   ENDTIME=$(date +%s)
   MINUTES=$(( ($ENDTIME - $STARTTIME) / 60 ))
   #echo "$MINUTES"
-  if [ "$MINUTES" -gt "$maxtime" ] # check if elapsed time is greater than max time allowed
-  then
+  if [ "$MINUTES" -gt "$maxtime" ] ; then # check if elapsed time is greater than max time allowed
     echo "Simulation $s failed"
-    #pkill -2 R # kill all R processes
-    #kill $(ps -o pid= --ppid $$)
+    for pid in "${PIDS[@]}"; do
+    pkill -P "$pid"
+    done
     pkill -P $$ #kill $(ps -s $$ -o pid=) # kills all children of the current given process $$
-    #for pid in "${PIDS[@]}"; do
-    #kill "$pid"
-    #done
+
   cd .. # return to main directory
   rm -R Simu$s # remove failed simulation
 	break       	   #Abandon the while loop
   fi
+  
   sleep 60;   # sleep for minute
-done
+  
+done # end while loop
 
 if [ "$(ls -A $DIR)" ]; then
 #if [ -f "$DIR/RES_Pop-14.RData" ]; then
     ENDTIME=$(date +%s)
     MINUTES=$(( ($ENDTIME - $STARTTIME) / 60 ))
+    maxtime=$(( $MINUTES + 10 ))    
     echo "Simulation $s succesful! Duration: $MINUTES minutes"
-    sleep 10 # sleep for 30 seconds
+    echo "New maxtime : $maxtime minutes"
+    sleep 30 # sleep for 30 seconds
     cd .. # return to main directory
 fi
 
